9d55dc49fe10115a65d3e5e222e726476a911006
Ilyin Vladislav <vlad.ilyin.2003@gmail.com>
2022-04-01 13:40:07 +0300

    Add crud proect and task

--- web_p/main/forms.py
+++ web_p/main/forms.py
@@ -1,3 +1,43 @@
 from .models import *
-from django import forms
+from django.forms import ModelForm, Textarea, TextInput, DateInput, SelectMultiple, CheckboxInput
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['header', 'description']
+        widgets = {
+            'header': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название проекта',
+            }),
+            'description': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Описание',
+            }),
+
+        }
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['header', 'description', 'deadline', 'members']
+        widgets = {
+            'header': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название проекта',
+            }),
+            'description': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Описание',
+            }),
+        }
+
+class TaskFileForm(ModelForm):
+    class Meta:
+        model = TaskFile
+        fields = ['file']
+        widgets = {
+        }
 
--- web_p/main/urls.py
+++ web_p/main/urls.py
@@ -4,4 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('home', views.HomeView.as_view(), name='home'),
+    path('my_projects', views.ProjView.as_view(), name='my_projects'),
+    path('my_projects/create', views.CreateProj.as_view(), name='my_projects_create'),
+    path('my_project/<int:id>', views.DetailProject.as_view(), name='my_project'),
+    path('my_project/<int:id>/update', views.UpdateProj.as_view(), name='my_project_update'),
+    path('my_project/<int:id>/delete', views.DeleteProj.as_view(), name='my_project_delete'),
+    path('my_project/<int:id>/create_task', views.CreateTask.as_view(), name='task_create'),
+    path('my_project/task/<int:id>', views.DetailTask.as_view(), name='task'),
+    path('my_project/task/<int:id>/update', views.UpdateTask.as_view(), name='task_update'),
+    path('my_project/task/<int:id>/delete', views.DeleteTask.as_view(), name='task_delete'),
 ]
